d33212d45faa51853640a4664aae816cb4a1e1f1
tonyreaves <tonyfreaves@gmail.com>
2018-07-30 10:25:44 -0700

    file structure in place

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+
+var burger = require("../models/burger.js");
+
+
+// Export routes for server.js to use.
+module.exports = router;
