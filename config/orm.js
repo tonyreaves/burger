b152fd298b9898a66e7c08e54cbe9953b39abc95
tonyreaves <tonyfreaves@gmail.com>
2018-08-04 10:08:52 -0700

    css changes, add burger

--- config/orm.js
+++ config/orm.js
@@ -29,7 +29,28 @@ var orm = {
         });
     },
 
-    insertOne: function(table, objColVals, condition, cb) {
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    updateOne: function(table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
     
         queryString += " SET ";
@@ -45,25 +66,7 @@ var orm = {
     
           cb(result);
         });
-      },
-
-    updateOne: function (table, id, condition, cb) {
-        var queryString = "UPDATE " + table;
-
-        queryString += " SET ";
-        queryString += true;
-        queryString += " WHERE ";
-        queryString += false;
-
-        console.log(queryString);
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-
-            cb(result);
-        });
-    },
+      }
 }
 
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -32,5 +32,16 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
 // Export routes for server.js to use.
 module.exports = router;
