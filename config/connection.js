f64548c1f9a3a05b6f0b8d181f48f4970ee8680a
tonyreaves <tonyfreaves@gmail.com>
2018-08-04 23:18:33 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,29 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+  //Heroku deployment
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+  connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
     password: "root",
     database: "burgers_db"
-  });
+  }
+  )
+};
 
-  // Make connection.
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-  // Export connection for our ORM to use.
-  module.exports = connection;
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
